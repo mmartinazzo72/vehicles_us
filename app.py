ca289f7fe1fa43419272ede9ba54527e236973b4
mmartinazzo72 <martinazzoim@yahoo.com>
2024-12-06 23:52:18 -0300

    atualizando o app com o botão de gráfico de dispersão

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.header('Analise Gráfica dos Anúncios de Veículos nos EUA')
 # criar um botão para gerar um histograma
 hist_button = st.button('Criar histograma')
 
-# se o botão for clicado
+# se o botão histograma for clicado
 if hist_button:
     
     # mensagem para que o usuário saiba o que está acontecendo
@@ -23,3 +23,18 @@ if hist_button:
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
 
+
+# criar um botão para gerar um gráfico de dispersão
+dispersao_button = st.button('Criar gráfico de dispersão')
+
+# se o botão gráfico de dispersão for clicado
+if dispersao_button:
+    
+    # mensagem para que o usuário saiba o que está acontecendo
+    st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+            
+    # criar um gráfico de dispersão com os dados de odometro vs price dos anúncios constantes no dataframe
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
